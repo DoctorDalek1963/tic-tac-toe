8a7b521fb55da13c9919c3e18b0651f0ee30a7f4
DoctorDalek1963 <dyson.dyson@icloud.com>
2024-05-22 22:35:53 +0100

    Use my fork of crane to speed up build times

--- flake.nix
+++ flake.nix
@@ -15,7 +15,8 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     crane = {
-      url = "github:ipetkov/crane";
+      # url = "github:ipetkov/crane";
+      url = "github:DoctorDalek1963/crane/parallelize-removeReferencesToVendoredSources";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
