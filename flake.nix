3ec856de2970b6226dbb90ce67a1bf5652ae1275
DoctorDalek1963 <dyson.dyson@icloud.com>
2024-05-27 21:53:43 +0100

    Update crane

--- flake.nix
+++ flake.nix
@@ -15,8 +15,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     crane = {
-      # url = "github:ipetkov/crane";
-      url = "github:DoctorDalek1963/crane/parallelize-removeReferencesToVendoredSources";
+      url = "github:ipetkov/crane";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
